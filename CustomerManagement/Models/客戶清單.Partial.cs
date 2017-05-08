b4772037e6cfc7b99b4bd0979a5353b659e89406
ewingtsui <ewing.tsui@gmail.com>
2017-05-08 22:33:39 +0800

    5.加入 EFGeneratePartialClass.tt , 自動產生 各資料模型的PartialClass

---
+++ CustomerManagement/Models/客戶清單.Partial.cs
@@ -0,0 +1,23 @@
+namespace CustomerManagement.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(客戶清單MetaData))]
+    public partial class 客戶清單
+    {
+    }
+    
+    public partial class 客戶清單MetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 客戶名稱 { get; set; }
+        public Nullable<int> 聯絡人數量 { get; set; }
+        public Nullable<int> 銀行數量 { get; set; }
+    }
+}
